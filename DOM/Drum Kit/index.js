21b65b356f636a408aad5a71878a5f0c14b72c77
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-11-02 18:24:46 +0530

    objects and methods

--- DOM/Drum Kit/index.js
+++ DOM/Drum Kit/index.js
@@ -2,5 +2,8 @@ for(var i=0; i<document.getElementsByClassName("drum").length; i++){
     document.getElementsByClassName("drum")[i].addEventListener("click",handleClick);
 }
 function handleClick(){
-    alert("I got clicked");
+    var alpha =this.innerHTML;
+    // this.style.color='white';
+    // var audio = new Audio("sounds/crash.mp3");
+    // audio.play();
 }
