667e1f8d1017c379859019027ee8a198ebe424c9
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-11-13 22:04:53 +0530

    jQuery

--- Mini Projects/jQuery/style.js
+++ Mini Projects/jQuery/style.js
@@ -1,4 +1,25 @@
 $("h1").css("color", "white");
-        $("h1").addClass("big-title new");
-        $("h1").text("Bye");
-        $("a").attr("href","github.com/geeky01adarsh");
+$("h1").addClass("big-title new");
+$("h1").text("Bye");
+$("a").attr("href", "github.com/geeky01adarsh");
+$("h1").click(function(){
+    $("h1").css("color", "purple");
+});
+
+$("button").click(function(){
+    // $("h1").animate({
+    //     opacity:0.5
+    // });
+
+    // $("h1").toggle();
+
+    // $("h1").slideToggle();
+
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
+
+$("input").keydown(function(event){
+    console.log(event.key);
+    $("h1").test(bhaiya,tem);
+});
+
