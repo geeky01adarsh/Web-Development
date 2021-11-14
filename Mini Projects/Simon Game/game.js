63b6effba640cd67d00c2a5ae1a0aac4890bc666
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-11-14 08:08:22 +0530

    simon game- third step

--- Mini Projects/Simon Game/game.js
+++ Mini Projects/Simon Game/game.js
@@ -5,3 +5,16 @@ function nextSequence(){
     var randomChosenColour = buttonColours[randomNo];
 }
 gamePattern.push(nextSequence());
+var id = 0;
+$("#"+gamePattern[id]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+const audio = new Audio("sounds/"+gamePattern[id]+".mp3");
+audio.play();
+// $('#'+gamePattern[id]).click(function() {
+    
+//   });
+
+// $("#red").click(function(){
+//     const audio = new Audio("sounds/"+gamePattern[id]+".mp3");
+//     audio.play();
+// });
+  
