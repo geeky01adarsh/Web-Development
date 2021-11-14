614e81ad6693596e49976bdf6614172f92166802
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-11-14 07:32:49 +0530

    second step

--- Mini Projects/Simon Game/game.js
+++ Mini Projects/Simon Game/game.js
@@ -1,1 +1,7 @@
-alert("hi");
+const buttonColours=["red", "blue", "green", "yellow"];
+const gamePattern= [];
+function nextSequence(){
+    var randomNo = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNo];
+}
+gamePattern.push(nextSequence());
