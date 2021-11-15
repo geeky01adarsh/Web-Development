f7878a13408f1a6fc1b29f07f89e8dae2d127f8c
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-11-15 09:43:28 +0530

    simon game completed

--- Mini Projects/Simon Game/game.js
+++ Mini Projects/Simon Game/game.js
@@ -26,47 +26,38 @@ $(".btn").click(function() {
   checkAnswer(userClickedPattern.length-1);
 });
 
-
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
           nextSequence();
         }, 1000);
-
       }
-
     } else {
-
       playSound("wrong");
-      $("body").addClass(game-over);  
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
       startOver();
-
     }
-
 }
+
 
 function nextSequence() {
   userClickedPattern = [];
-
   level++;
   $("#level-title").text("Level " + level);
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-}
-
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
 }
 
 function animatePress(currentColor) {
@@ -76,9 +67,13 @@ function animatePress(currentColor) {
   }, 100);
 }
 
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
-function startOver(){
-  level=0;
+function startOver() {
+  level = 0;
   gamePattern = [];
-  started=false;
+  started = false;
 }
