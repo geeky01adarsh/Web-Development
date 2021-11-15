64ec1ea6a13c43f4fa302660ff38794c4f12ecd8
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-11-15 07:26:36 +0530

    step 6 done

--- Mini Projects/Simon Game/game.js
+++ Mini Projects/Simon Game/game.js
@@ -1,20 +1,46 @@
-const buttonColours=["red", "blue", "green", "yellow"];
-const gamePattern= [];
-function nextSequence(){
-    var randomNo = Math.floor(Math.random()*4);
-    var randomChosenColour = buttonColours[randomNo];
+const buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+function playSound(track){
+    const audio = new Audio("sounds/"+track+".mp3");
+    audio.play();
 }
-gamePattern.push(nextSequence());
-var id = 0;
-$("#"+gamePattern[id]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-const audio = new Audio("sounds/"+gamePattern[id]+".mp3");
-audio.play();
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#"+currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function nextSequence() {
+  var randomNo = Math.floor(Math.random() * 4);
+  var id = buttonColours[randomNo];
+  gamePattern.push(nextSequence());
+  $("#" + gamePattern[id])
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(gamePattern[id]);
+}
+
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+//   console.log(userClickedPattern);
+  animatePress(userChosenColour);
+});
+
+
 // $('#'+gamePattern[id]).click(function() {
-    
+
 //   });
 
 // $("#red").click(function(){
 //     const audio = new Audio("sounds/"+gamePattern[id]+".mp3");
 //     audio.play();
 // });
-  
